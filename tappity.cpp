d2dea71fdf05c56db3977b4ba9bc4290e030b725
manicdepravity <andrewjh1986@outlook.com>
2022-12-04 20:24:30 -0800

    Added function requirements for quiz as well as tests to prove correct implementation

--- tappity.cpp
+++ tappity.cpp
@@ -1,40 +1,80 @@
 #include "tappity.h"
+#include <iostream>
 /**
  * Class for tappity
 **/
 
-//Constructor sets the reference phrase
-tappity::tappity(std::string reference)
-{
+using namespace std;
+
+tappity::tappity(){
+    tappity::strInput = "";
+    tappity::strReference = "";
 }
 
-//Provide the input to be compared to the reference. Before this 
+//////Constructor sets the reference phrase
+tappity::tappity(std::string reference){tappity::strReference = reference;}
+
+//Provide the input to be compared to the reference. Before this
 //function is called, the input should be considered an empty string
-void tappity::entry(std::string input)
-{
-}
+void tappity::entry(std::string input){tappity::strInput = input;}
 
 //Compares the length of the reference to that of the input and
 //returns the absolute value of the difference, or 0 if they are the
 //same length
 int tappity::length_difference()
-{
-  return 0;
+{   std::string input = tappity::strInput;
+    std::string reference = tappity::strReference;
+    int inLength = input.length();
+    int refLength = reference.length();
+
+    if(inLength >= refLength){
+        return abs(inLength-refLength);
+    }
+    else
+        return abs(refLength-inLength);
 }
 
 //Compares the content of the reference to that of the input and
 //calculates the accuracy of matching characters. If the two strings
-//are identical, accuracy should be 100. However, for each 
+//are identical, accuracy should be 100. However, for each
 //corresponding character that does not match in the same location,
 //the percentage of corresponding characters should be returned.
 //For example, if the reference and input have 10 letters and the input
-//matches 8 of the corresponding reference characters, the function 
+//matches 8 of the corresponding reference characters, the function
 //should return 80. When input does not have the same number of
 //characters as the reference, the accuracy should represent the percent
 //of matching characters between the shorter and longer strings. For
 //example, if one string has 8 characters matching the corresponding
 //locations in another string that has 16 characters, the accuracy is 50.
-double tappity::accuracy()
-{
-  return 0;
+double tappity::accuracy() {
+    std::string input = tappity::strInput;
+    std::string reference = tappity::strReference;
+    int sizeDiff = length_difference();
+    double nullVal = 100.0;
+    double temp = 0.0;
+    auto count = 0.0;
+    //Base Case Empty is a Match
+    if (input == "" && reference == "") return nullVal;
+
+    // set the index of smaller and larger arrays
+    if (input.length() >= reference.length()) {
+        temp = input.length();
+        for (auto i = 0; i < input.length() - sizeDiff; i++) {
+            if (input[i] == reference[i])count += 1;// if match found +1
+
+        }
+    }
+    else
+        {
+        temp = reference.length();
+        // line index of larger possible array as start and ending point
+        for (auto i = 0; i < reference.length() - sizeDiff; i++) {
+            if (input[i] == reference[i])count += 1;  // if match found +1
+
+            }
+        }
+    // Remove the decimal places to return in integer form
+    int rounded = (count/temp) *100;
+    return rounded;
+
 }
--- tappityTest.cpp
+++ tappityTest.cpp
@@ -13,3 +13,189 @@ class tappityTest : public ::testing::Test
 		virtual void SetUp(){}
 		virtual void TearDown(){}
 };
+
+
+//Happpy Test Constructor
+TEST(tappityTest, construct)
+{
+    // Testt that the constructor is set by comparing the string to Constructor length
+    tappity construct("Test");
+    std::string testIt = "Test";
+
+    // Test Should Pass Proving the COnstructor is set
+    ASSERT_EQ(construct.length_difference(), testIt.length());
+
+}
+
+//Sad Test Constructor and Entry
+TEST(tappityTest, constructWrongLength)
+{
+    // Test that the constructor is of actual lenght four by proving it will fail when not equated properly
+    tappity constructWrongLength("Test");
+    std::string testIt = "TestWrongLenth";
+    ASSERT_EQ(constructWrongLength.length_difference(), testIt.length());
+}
+
+//Happy Test Entry
+TEST(tappityTest, entryTestHappy)
+{
+    // Test the Lenght is the same
+    tappity entryTestHappy("Hello111");
+    //entryTestHappy.entry("Hello111");
+    entryTestHappy.entry("Hello111");
+    ASSERT_EQ(entryTestHappy.length_difference(), 0);
+}
+
+//Sad Test Entry
+TEST(tappityTest, entryTestSad)
+{
+    // Test length will fail for equality is the arrays are of differnet size
+    tappity entryTestSad("This test will fail");
+    entryTestSad.entry("This test will Fail");
+    ASSERT_EQ(entryTestSad.accuracy(), 100);
+}
+
+
+//Happy Test Accuracy Blank Matching
+TEST(tappityTest, is_blank_matched)
+{
+    // Test that blanks are counted
+    tappity is_blank_matched("     fiveblanks");
+    is_blank_matched.entry("     fiveblanks");
+    int accScore = is_blank_matched.accuracy();
+    int fullMatch = 100;
+
+    ASSERT_TRUE(accScore == fullMatch);
+}
+
+//Sad Test Accuracy Blanks Do not Match
+TEST(tappityTest, blanks_do_not_match)
+{
+    // Test that blanks after last word are differrent
+    tappity blanks_do_not_match("     five");
+    blanks_do_not_match.entry("     five ");
+    int accScore = blanks_do_not_match.accuracy();
+    int fullMatch = 100;
+
+
+    ASSERT_EQ(accScore , fullMatch);
+}
+
+//Happy Test Accuracy Count Blanks Only
+TEST(tappityTest, countBlanks)
+{
+    // Test that acuraCY IS  correct for 50 match of blanks
+    tappity countBlanks(" ");
+    countBlanks.entry("  ");
+
+    int accScore  = countBlanks.accuracy();
+    int fullMatch = 50;
+
+    ASSERT_EQ(accScore , fullMatch);
+}
+
+//Sad Test Accuracy Count BLanks Only
+TEST(tappityTest, countBlanks_noMatch)
+{
+    // Test that accuracy falls for a 100 percent match as the actual is 50 %
+    tappity countBlanks_noMatch(" ");
+    countBlanks_noMatch.entry("  ");
+
+    int accScore = countBlanks_noMatch.accuracy();
+    int Match = 100;
+
+    ASSERT_EQ(accScore , Match);
+}
+
+//Happy Test Numbers
+TEST(tappityTest, matchNumbers)
+{
+    // Test that numbers are matched
+    tappity matchNumbers("123123");
+    matchNumbers.entry("123123");
+
+    int accScore = matchNumbers.accuracy();
+    int fullMatch = 100;
+
+    ASSERT_EQ(accScore , fullMatch);
+}
+
+//Sad Test Numbers
+TEST(tappityTest, noMatchNumbers)
+{
+    // Test that accuracy accounts for end of array mismatch
+    tappity noMatchNumbers("1231231");
+    noMatchNumbers.entry("12312312");
+
+    int accScore = noMatchNumbers.accuracy();
+    int fullMatch = 100;
+
+    ASSERT_EQ(accScore , fullMatch);
+}
+
+//Happy Test Empty is can match
+TEST(tappityTest, emptyMatch)
+{
+    // Test for BAse Case
+    tappity emptyMatch("");
+    emptyMatch.entry("");
+
+    int accScore = emptyMatch.accuracy();
+    int fullMatch = 100;
+
+    ASSERT_EQ(accScore , fullMatch);
+}
+
+//Happy Test case sensitivity Match
+TEST(tappityTest, caseSizeMatters)
+{
+    // Test for Case Sensitivity
+    tappity caseSizeMatters("CASEsensitivity");
+    caseSizeMatters.entry("CASEsensitivity");
+
+    int accScore = caseSizeMatters.accuracy();
+    int fullMatch = 100;
+
+    ASSERT_EQ(accScore , fullMatch);
+}
+
+//Sad Test case sensitivity Match
+TEST(tappityTest, caseSizeSameCheck)
+{
+    // Test the case sensitivity will fail if mismatched
+    tappity caseSizeSameCheck("CASesensitivitY");
+    caseSizeSameCheck.entry("CASesensitivity");
+
+    int accScore = caseSizeSameCheck.accuracy();
+    int fullMatch = 100;
+
+    ASSERT_EQ(accScore, fullMatch);
+}
+
+//Happy Test length DIfference
+TEST(tappityTest, lengthSizeSame)
+{
+    // Test the length comparison function for exact match
+    tappity lengthSizeSame("SameSizeLength");
+    lengthSizeSame.entry("SameSizeLength");
+
+    int length = lengthSizeSame.length_difference();
+
+    int zeroDifference = 0;
+
+    ASSERT_EQ(length, zeroDifference);
+}
+
+//Sad Test length DIfference
+TEST(tappityTest, lengthSizeNotSame)
+{
+    // Test is length comparison not always returning 0
+    tappity lengthSizeNotSame("NotSameLenghth");
+    lengthSizeNotSame.entry("NotSameLenghthSee");
+
+    int length = lengthSizeNotSame.length_difference();
+
+    int zeroDifference = 0;
+    ASSERT_EQ(length, zeroDifference);
+
+}
